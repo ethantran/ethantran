82c818a529c1288548ef5896631378baa482fa96
Ethan Tran <ethan.tran@7-11.com>
2019-06-23 22:17:14 -0500

    Update index

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './all.sass'
+import './all.css'
 import useSiteMetadata from './SiteMetadata'
 
 const TemplateWrapper = ({ children }) => {
@@ -13,40 +11,30 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-16x16.png"
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
-
+        <link rel="apple-touch-icon" sizes="57x57" href="/img/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/img/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/img/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/img/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/img/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/img/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/img/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/img/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png"/>
+        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ffffff" />
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ffffff"/>
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
         <meta property="og:image" content="/img/og-image.jpg" />
       </Helmet>
-      <Navbar />
       <div>{children}</div>
-      <Footer />
     </div>
   )
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
 
 export const IndexPageTemplate = ({
   image,
@@ -13,106 +13,42 @@ export const IndexPageTemplate = ({
   subheading,
   mainpitch,
   description,
-  intro,
+  intro
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
+    <div className="bg flex object-center content-center items-center justify-center">
+      <div className="profile object-contain h-48 w-48" />
     </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
+    <div className="w-full max-w-screen-xl mx-auto px-6">
+      <div className="lg:flex -mx-6">
+        <div className="min-h-screen w-full lg:static lg:max-h-full lg:overflow-visible">
+          <div className="flex">
+            <div className="pt-24 pb-16 lg:pt-28 w-full">
+              <div className="content px-6 py-6">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-4xl font-light leading-tight">
+                  Hey, I'm Ethan Tran. I love to experiment and solve problems
+                  with React.
+                </h1>
+                <h2>Portfolio</h2>
+                <h2>Experiments</h2>
+                <h2>Thoughts</h2>
+                <h2>Dance Monkey Dance!</h2>
+                <a
+                  className="navbar-item"
+                  href="https://www.youtube.com/watch?v=81sOzZZgBwI"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  CHAIYA CHAIYA
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -122,12 +58,12 @@ IndexPageTemplate.propTypes = {
   mainpitch: PropTypes.object,
   description: PropTypes.string,
   intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+    blurbs: PropTypes.array
+  })
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,18 +77,18 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +126,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
